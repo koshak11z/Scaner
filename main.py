f4ae11335cd2407732a1d04e339fad8321c81a8d
koshak11z <166287330+koshak11z@users.noreply.github.com>
2024-04-10 18:54:47 +0300

    обновление на Scaner
    убрал у вас были найдёны читы, добавил в папки файлов (кстати баритон почему то не робит не пытался пофиксить не получалось)
    чуть упростил код

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 import getpass
 import time
+import zipfile
 
 # Определение Username
 username = getpass.getuser()
 
-# Открытие
+# Открытие папок
 os.startfile('C:/Windows/Temp')
 os.startfile('C:/Program Files')
 os.startfile(f'C:/Users/{username}/AppData/Roaming/.tlauncher/legacy/Minecraft/game')
 os.startfile(f'C:/Users/{username}/AppData/Roaming/.minecraft')
 os.startfile('C:/Program Files (x86)')
-os.startfile('C:')
+os.startfile('C:/')
 
 # Пути
 folder_path = 'C:/Nurik/java_client_1.16.5/configs'
@@ -19,31 +20,39 @@ folder_path2 = 'C:/Nurik/java'
 
 # Проверка 
 if not os.path.exists(folder_path):  
-    os.makedirs(folder_path)  # Создание директории 
+    os.makedirs(folder_path) 
 
-# Создание json в конфиг
-file_path = os.path.join(folder_path, 'pena.json')  
+# Создание настроек в опционах
+options_file_path = os.path.join('C:/Nurik/java_client_1.16.5', 'options.txt')
+with open(options_file_path, 'w') as options_file:
+    options_file.write("fov = 1.0\n")
+    options_file.write("gamma = 100000\n")
+    options_file.write("max_fps = V-SYNC\n")
+    options_file.write("fullscreen = true\n")
+    
+# Создание ассетсов
+assets_folder_path = os.path.join('C:/Nurik/java_client_1.16.5', 'assets')
+assets_zip_path = os.path.join('C:/Nurik/java_client_1.16.5', 'assets.zip')
+with zipfile.ZipFile(assets_zip_path, 'w') as zipf:
+    for root, _, files in os.walk(assets_folder_path):
+        for file in files:
+            zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), assets_folder_path))
 
-with open(file_path, 'w'):  # Создание пустого файла
-      pass
+# Создание minecraft.jar
+minecraft_jar_path = os.path.join('C:/Nurik/java_client_1.16.5', 'minecraft.jar')
+with open(minecraft_jar_path, 'w'): 
+    pass
 
-# Создание всех папок
-additional_folders = ['saves', 'logs', 'baritone', 'libraries', 'ViaFabric', 'PlayerModels', 'resourcepack', 'natives', 'assets']
+# Создание баритона
+baritone_folder_path = os.path.join(folder_path, 'baritone')
+if not os.path.exists(baritone_folder_path):
+    os.makedirs(baritone_folder_path)
+
+
+# Создание остальных папок
+additional_folders = ['saves', 'logs', 'libraries', 'ViaFabric', 'PlayerModels', 
+                      'resourcepack', 'natives', 'baritone']
 for folder in additional_folders:
     folder_path = os.path.join('C:/Nurik/java_client_1.16.5', folder) 
     if not os.path.exists(folder_path):  
         os.makedirs(folder_path) 
-
-
-# Создание Nursultan в диск ц
-for folder in additional_folders:
-    folder_path = os.path.join('C:/', folder)
-
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-
-print("У вас были найдены читы... Путь до чита C:/Nurik")
-time.sleep(10000)
-exit()
-
